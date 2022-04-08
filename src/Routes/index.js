3768c2dd50ab460c2201fd0861c3d8ef09dc1006
Arthur <arthurarp@hotmail.com>
2022-04-08 18:19:17 -0300

    [#3] - Add routes to project
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Rafaella Junqueira <rafaellafjunqueira@gmail.com>
    Signed-off-by: Arthur <arthurarp@hotmail.com>

---
+++ src/Routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../pages/Home';
+import Connection from '../pages/Connection';
+
+export default function Routes() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Connection"
+          component={Connection}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
