1e69f0b9728f1e92688e1493e888fcffacf9a3b7
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2022-04-27 19:38:16 -0300

    [fix] Correcao do cronometro
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>

--- src/pages/Stopwatch/index.js
+++ src/pages/Stopwatch/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import normalize from 'react-native-normalize';
 import ScreenTemplate from '../../components/ScreenTemplate';
 import AlertComponent from '../../components/Alert'
@@ -13,23 +13,23 @@ import {
 } from './style';
 
 function Stopwatch({navigation}) {
-    // const [minutes, setMinutes] = useState(4)
-    const [seconds, setSeconds] = useState(120)
-    var convert = require('convert-seconds');
-
-
-    const timer = setInterval ((async) => {
-        // setSeconds(seconds-1)
-        // if (minutes === 0 && seconds === 0){
-        //     clearTimeout(timer)
-        // }
-        // while (minutes !== 0 && seconds === 0){
-        //     setMinutes(minutes-1)
-        //     setSeconds(59)
-        // }
-        // console.log("ANDALOGO"); 
-    }, 10000)
-
+    // Inicia um valor pros segundos
+    const [seconds, setSeconds] = useState(10);
+    const intervalRef = useRef();
+      
+    useEffect(() => {
+      intervalRef.current = setInterval(() => {
+        setSeconds((t) => t - 1);
+      }, 1000);
+      return () => clearInterval(intervalRef.current);
+    }, []);
+    
+    useEffect(() => {
+      if (seconds <= 0) {
+        clearInterval(intervalRef.current);
+      }
+    }, [seconds]);
+    
   const navigateTo = (screenName) => {
     navigation.navigate(screenName);
   };
@@ -38,15 +38,15 @@ function Stopwatch({navigation}) {
       MainText={"Resfriando"}
       buttonColor="#BC2A2A"
       buttonText="Interromper"
-      onPress={() => clearInterval(timer)}
+      onPress={() => {clearInterval(intervalRef.current)}}
     >
       <Container>
           <StopwatchView>
           <Icon name="alarm" size={normalize(35)} color={"#7c7c7c"}/>
             <StopwatchText>
-               {/* {`${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}`} */}
-               {/* {`${convert(seconds).minutes}`} */}
-               {seconds}
+               {/* {${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}} */}
+               {/* {${convert(seconds).minutes}} */}
+               {`${seconds}:${seconds}`}
             </StopwatchText>
           </StopwatchView>
         <TextView>
